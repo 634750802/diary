592f9b49f710780d48ec7417ccd754f2221eb213
Jagger <linjie.gao@tendcloud.com>
2019-07-16 11:46:49 +0800

    first commit

---
+++ .circleci/deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn build
+
+# navigate into the build output directory
+cd .vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+
+git config user.email "634750802@qq.com"
+git config user.name "634750802(CI)"
+
+git add -A
+git commit -m 'Deploy docs'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:634750802/diary master:gh-pages
+
+cd -
